4d228b43b69d4bf8925c0ef88ebde9f363926628
Fahrij Arrohman <fahrij06@gmail.com>
2025-02-11 17:00:45 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.uas_fahrij;
 import java.util.Scanner;
 import java.util.ArrayList;
